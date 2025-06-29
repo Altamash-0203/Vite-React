d2dd8b4495c2551581f5aefe9a66f6b63b903888
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-06-29 18:26:02 +0530

    quote generator

---
+++ src/UseEffect_Hook/Q2.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+
+function Quotes(){
+
+    let [quote,setquote]=useState("")
+    let [load,setload]=useState(false)
+
+
+   async function fetchQuote(){
+    setload(true)
+
+    try{
+        let res=await fetch( "https://favqs.com/api/qotd")
+
+        let data=await res.json()
+
+        setquote(data.quote.body)
+        console.log(quote)
+        setload(false)
+    }
+    catch(error){
+        console.log(error.message)
+        setload(false)
+    }
+   }
+   
+       useEffect(()=>{
+           fetchQuote()
+
+           let wait=setInterval(() => {
+            fetchQuote()
+           }, 30000);
+         
+           return()=>clearInterval(wait)
+       },[])
+
+      
+       return(
+
+       <>
+       <div>
+        {load && <p>Loading...</p>}
+       </div>
+         <div>
+              <h3>{quote}</h3>
+         </div>
+         <button onClick={fetchQuote}>
+            new 
+         </button>
+       </>
+
+       )
+}
+
+export default Quotes
